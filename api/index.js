e368dbdaeb803ae6204d1d6a8f154bb29621cd63
felipetodev <fe.ossandon.u@gmail.com>
2023-01-07 22:17:15 -0300

    add api route for specific area

--- api/index.js
+++ api/index.js
@@ -7,13 +7,29 @@ app.get('/', (ctx) => {
   return ctx.json([
     {
       endpoint: '/apartments',
-      description: 'Returns all apartments'
+      description: 'Returns all apartments',
+      parameters: [
+        {
+          name: 'area',
+          endpoint: '/apartments/:area',
+          description: 'Return Apartments by area'
+        }
+      ]
     }
   ])
 })
 
 app.get('/apartments', (ctx) => {
   return ctx.json(apartments)
+})
+
+app.get('/apartments/:area', (ctx) => {
+  const area = ctx.req.param('area')
+  const foundApartmentArea = apartments.filter(({ slug }) => slug.includes(area))
+
+  return foundApartmentArea.length > 0
+    ? ctx.json(foundApartmentArea)
+    : ctx.json({ message: 'Area not found' }, 404)
 })
 
 export default app
